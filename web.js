1e26ff17c6d54802a8971426c969d27ca21634a3
Chirayush Agarwal <thechirayush@gmail.com>
2013-07-11 11:22:20 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -3,10 +3,20 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+    response.send(getIndexData());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var getIndexData = function() {
+    var data = fs.readFileSync("index.html");
+//    console.log(data);
+//    console.log(data.length);
+    var ds = data.toString("utf-8", 0, data.length-1);
+    console.info(ds);
+    return ds;
+}
